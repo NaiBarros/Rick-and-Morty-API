64f4e5ba228c4d89fffb26cfba2059821d71997e
Naiara Barros <naiara.b.barros@gmail.com>
2022-06-14 21:42:59 -0300

    Atualizando o javascript

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let nomeElement = document.getElementById("nome")
+let listaPersonagensElement = document.getElementById("listaPersonagens")
 
-
-async function getPersonagem() {
-  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+async function getPersonagens() {
+  const response = await fetch('https://rickandmortyapi.com/api/character');
   const data = await response.json()
 
-  personagens = data.results;
+  const personagens = data.results;
 
+  personagens.forEach(personagem => {
+    let novoElemento = document.createElement('li')
+
+    novoElemento.classList.add('card')
+
+    novoElemento.innerHTML = `
+      <figure>
+        <img class="imgCard" src=${personagem.image} alt="">
+      </figure>
+      <div class="informaçoes">
+        <div>
+          <h2>${personagem.name}</h2>
+        </div>
+        <h3>${personagem.status} - ${personagem.species}</h3>
+        <div class="info2">
+          <h3>Last known location: </h3>
+          <p>${personagem.location.name}</p>
+          <h3>First seen in: </h3>
+          <p>Total Rickall</p>
+        </div>
+      </div>
+    `
+
+    listaPersonagensElement.appendChild(novoElemento)
+  })
 }
 
-getPersonagem()
+getPersonagens()
